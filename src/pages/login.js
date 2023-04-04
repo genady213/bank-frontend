eed7f4caf440de3a81947e5f2a4d90c1c626c6fc
Genady <genady213@gmail.com>
2023-04-04 00:40:09 -0400

    added axios

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,46 @@
 import { useRef, useEffect } from 'react';
 import './login.css';
+import axios from '../axios';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useNavigate,
+} from 'react-router-dom';
 
 export function Login() {
   const myuser = useRef('');
   const mypass = useRef('');
+  let navigate = useNavigate();
+
+
+  async function sendData(user, pass) {
+    var theans = "";
+    const req = await axios.post('/login', {"username":user,"password":pass})
+    .then((response) => {
+      console.log(response);
+      theans = response.data.message;
+      //token = response.data.token;
+      //Cookies.set('userid', response.data.id);
+      //Cookies.set('username', user);
+      //apiClient.defaults.headers.common["Authorization"] = token;
+    return theans;
+    }, (error) => {
+      console.log(error);
+    });
+    return theans;
+  }
+  const routeChange = () => {
+    let path = `/Home`;
+    navigate(path);
+  };
+  const routeChangeRegister = () => {
+    let path = `/Register`;
+    navigate(path);
+  };
+
       return (
         <div className="login-main">
             <div className="login-top">
@@ -29,6 +66,19 @@ export function Login() {
                   className="login-button"
                   type="button"
                   value="Submit"
+                  onClick={async () => {
+                    const theToken = await sendData(
+                      myuser.current.value,
+                      mypass.current.value
+                    );
+                    if (theToken == 'Success') {
+                      //Cookies.set('token', token);
+                      routeChange();
+                    } else {
+                      document.getElementById('error').style.visibility =
+                        'visible';
+                    }
+                  }}
                 >
                   Login
                 </button>
